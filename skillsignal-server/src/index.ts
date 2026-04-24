ea1b4fe83fe93534bb046deb539af3e4bb3481b5
SHAUNAK SIKDAR <shaunaksikdar@gmail.com>
2026-04-24 16:09:17 +0530

    updated index.ts

--- skillsignal-server/src/index.ts
+++ skillsignal-server/src/index.ts
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 5000
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://skillsignal-one.vercel.app/', // add after you get Vercel URL
+  ],
   credentials: true,
 }))
 app.use(express.json())
